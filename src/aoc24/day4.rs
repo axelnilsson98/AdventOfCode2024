a9efb59eb25e9df230dc086e57b9ff5ce1b38f0b
Axel <ax1053ni-s@student.lu.se>
2024-12-04 08:15:14 +0100

    day 4 refactoring

--- src/aoc24/day4.rs
+++ src/aoc24/day4.rs
@@ -1,9 +1,11 @@
 use std::usize;
 
+type Dimensions = (usize, usize);
+
 pub fn part1(lines: Vec<String>) {
+    let dim: Dimensions = (lines.len(), lines[0].len());
+    let matrix: Vec<Vec<char>> = lines.iter().map(|str| str.chars().collect()).collect();
     let mut num = 0;
-    let dim: (usize, usize) = (lines.len(), lines[0].len());
-    let matrix: Vec<Vec<char>> = lines.into_iter().map(|str| str.chars().collect()).collect();
     for l in 0..matrix.len() {
         for c in 0..matrix[l].len() {
             if matrix[l][c] == 'X' {
@@ -15,8 +17,7 @@ pub fn part1(lines: Vec<String>) {
     }
     println!("{}", num);
 }
-fn num_xmas(l: usize, c: usize, m: &Vec<Vec<char>>, d: (usize, usize)) -> i32 {
-    let mut num = 0;
+fn num_xmas(l: usize, c: usize, m: &Vec<Vec<char>>, d: Dimensions) -> i32 {
     let dirs: [(i32, i32); 8] = [
         (1, 0),
         (1, 1),
@@ -27,22 +28,28 @@ fn num_xmas(l: usize, c: usize, m: &Vec<Vec<char>>, d: (usize, usize)) -> i32 {
         (0, -1),
         (1, -1),
     ];
-    // let mut dir: (i32, i32);
-    for (ll, cc) in dirs {
-        if get_cord(l, ll, 3) >= 0
-            && get_cord(l, ll, 3) < d.0 as i32
-            && get_cord(c, cc, 3) >= 0
-            && get_cord(c, cc, 3) < d.1 as i32
-        {
-            if m[get_cord(l, ll, 1) as usize][get_cord(c, cc, 1) as usize] == 'M'
-                && m[get_cord(l, ll, 2) as usize][get_cord(c, cc, 2) as usize] == 'A'
-                && m[get_cord(l, ll, 3) as usize][get_cord(c, cc, 3) as usize] == 'S'
-            {
-                num += 1;
-            }
-        }
-    }
+
+    let num = dirs
+        .iter()
+        .filter(|&&(ll, cc)| {
+            let l1 = get_cord(l, ll, 1);
+            let c1 = get_cord(c, cc, 1);
+            let l2 = get_cord(l, ll, 2);
+            let c2 = get_cord(c, cc, 2);
+            let l3 = get_cord(l, ll, 3);
+            let c3 = get_cord(c, cc, 3);
+
+            is_within_bounds(l3, c3, d)
+                && m[l1 as usize][c1 as usize] == 'M'
+                && m[l2 as usize][c2 as usize] == 'A'
+                && m[l3 as usize][c3 as usize] == 'S'
+        })
+        .count() as i32;
     num
+}
+
+fn is_within_bounds(x: i32, y: i32, d: Dimensions) -> bool {
+    x >= 0 && x < d.0 as i32 && y >= 0 && y < d.1 as i32
 }
 
 pub fn part2(lines: Vec<String>) {
@@ -62,30 +69,30 @@ pub fn part2(lines: Vec<String>) {
 }
 
 fn is_x_mas(l: usize, c: usize, m: &Vec<Vec<char>>, d: (usize, usize)) -> bool {
-    let dirs: [((i32, i32), (i32, i32)); 4] = [((1, 1), (-1, -1)), ((-1, 1), (1, -1)), ((-1, -1), (1, 1)), ((1, -1), (-1, 1))];
-    let mut num = 0;// let mut dir: (i32, i32);
-    for ((ml, mc), (sl, sc)) in dirs {
-        if is_inside(l, c, d, (ml, mc) )&& is_inside(l, c, d, (sl, sc))
-        {
-            if m[get_cord(l, ml, 1) as usize][get_cord(c, mc, 1) as usize] == 'M'
-                && m[get_cord(l, sl, 1) as usize][get_cord(c, sc, 1) as usize] == 'S'
-            {
-                // println!("xmas found dirs: ml: {}, mc: {}, sl: {}, sc: {}", ml, mc, sl, sc);
-                num +=1;
-            }
-        }
-    }
-    num ==2
+    let dirs: [((i32, i32), (i32, i32)); 4] = [
+        ((1, 1), (-1, -1)),
+        ((-1, 1), (1, -1)),
+        ((-1, -1), (1, 1)),
+        ((1, -1), (-1, 1)),
+    ];
+    let num: i32 = dirs
+        .iter()
+        .filter(|((ml, mc), (sl, sc))| {
+            is_inside(l, c, d)
+                && m[get_cord(l, *ml, 1) as usize][get_cord(c, *mc, 1) as usize] == 'M'
+                && m[get_cord(l, *sl, 1) as usize][get_cord(c, *sc, 1) as usize] == 'S'
+        })
+        .count() as i32;
+    num == 2
 }
 
 fn get_cord(x: usize, dir: i32, steps: i32) -> i32 {
-    // println!("x: {}, dir: {}, steps: {}", x, dir, steps);
     x as i32 + steps * dir
 }
 
-fn is_inside(l: usize, c: usize, d: (usize, usize), dir:(i32, i32))-> bool{
-    get_cord(l, dir.0, 1) >= 0
-    && get_cord(l, dir.0, 1) < d.0 as i32
-    && get_cord(c, dir.1, 1) >= 0
-    && get_cord(c, dir.1, 1) < d.1 as i32
+fn is_inside(l: usize, c: usize, d: (usize, usize)) -> bool {
+    get_cord(l, -1, 1) >= 0
+        && get_cord(l, 1, 1) < d.0 as i32
+        && get_cord(c, -1, 1) >= 0
+        && get_cord(c, 1, 1) < d.1 as i32
 }
