5efff5d5e6b7957ac45ad4a3a5838a008d1ba039
Axel <ax1053ni-s@student.lu.se>
2024-12-06 08:26:03 +0100

    day6

--- src/aoc24/day6.rs
+++ src/aoc24/day6.rs
@@ -1,7 +1,96 @@
-pub fn part1(lines: Vec<String>){
+use core::panic;
+use std::{collections::HashSet, usize};
 
+pub fn part1(lines: Vec<String>) {
+    let mut pos: (usize, usize) = (0, 0);
+    let mut dir: (i32, i32) = (-1, 0);
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let map = lines.iter().enumerate().map(|(i, line)| {
+        line.chars().enumerate().map(|(j, char)| {
+            if char == '^' {pos = (i,j)}
+            char != '#'
+        }).collect()
+    }).collect();
+
+    while in_bounds(&map, pos) {
+        visited.insert(pos);
+        if !value(&map, pos, dir){
+            dir = turn_right(dir);
+        } else {
+
+            pos = (cord(pos.0, dir.0), cord(pos.1, dir.1))
+        }
+    }
+    println!("{}", visited.len());
 }
 
-pub fn part2(lines: Vec<String>){
+fn in_bounds(map: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
+    !(pos.0 >= (map.len()) || pos.1 >= (map[0].len()))
+}
 
+fn value(map: &Vec<Vec<bool>>, pos: (usize, usize), dir: (i32, i32)) -> bool{
+    match map.get(cord(pos.0, dir.0)).and_then(|row| row.get(cord(pos.1, dir.1))){
+        Some(value) => return value.clone(),
+        None => return true
+    }
+}
+
+fn cord(p: usize, d: i32) -> usize {
+    (p as i32 + d) as usize
+}
+
+fn turn_right(dir: (i32, i32)) -> (i32, i32) {
+    match dir {
+        (-1, 0) => return (0, 1),
+        (0, 1) => return (1, 0),
+        (1, 0) => return (0, -1),
+        (0, -1) => return (-1, 0),
+        _ => panic!("wrong turn"),
+    }
+}
+pub fn part2(lines: Vec<String>) {
+    let mut pos: (usize, usize) = (0, 0);
+    let mut dir: (i32, i32) = (-1, 0);
+
+    let map: Vec<Vec<bool>> = lines.iter().enumerate().map(|(i, line)| {
+        line.chars().enumerate().map(|(j, char)| {
+            if char == '^' {pos = (i,j)}
+            char != '#'
+        }).collect()
+    }).collect();
+
+    let mut sum = 0;
+    for (i, line) in lines.iter().enumerate() {
+        for (j, char) in line.chars().enumerate() {
+            if char == '^' || char == '#' {
+                continue;
+            }
+            let mut tmp = map.clone();
+            tmp[i][j] = false;
+            if creates_loop(&tmp, pos, dir) {
+                sum += 1;
+            }
+        }
+        println!("{}", i);
+    }
+    println!("{}", sum);
+}
+
+fn creates_loop(map: &Vec<Vec<bool>>, start_pos: (usize, usize), start_dir: (i32, i32)) -> bool {
+    let mut visited: HashSet<((usize, usize), (i32, i32))> = HashSet::new();
+    let mut dir: (i32, i32) = start_dir;
+    let mut pos: (usize, usize) = start_pos;
+
+    while in_bounds(&map, pos) && !visited.contains(&(pos, dir)) {
+        visited.insert((pos, dir));
+        // println!("{:?}", pos);
+        if !value(&map, pos, dir) {
+            dir = turn_right(dir);
+        } else {
+            pos = (cord(pos.0, dir.0), cord(pos.1, dir.1))
+        }
+    }
+    let tmp = in_bounds(&map, pos);
+    // println!("{}", tmp);
+    tmp
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_assignments)]
 mod aoc24; 
 
 use std::fs::File;
@@ -8,14 +10,14 @@ use std::io::{self, BufRead, BufReader, Read};
 
 fn main() -> io::Result<()>{
 
-    let file_path: &str = "/home/axel/Documents/games/aoc24/src/input/day5.txt";
+    let file_path: &str = "/home/axel/Documents/games/aoc24/src/input/test.txt";
     let file: File = File::open(file_path)?;
     let reader: BufReader<File> = io::BufReader::new(file);
     let lines: Vec<String> = reader.lines()
         .map(|line| line.unwrap()) // Handle Result by unwrapping (use ? if errors should propagate)
         .collect();
 
-    aoc24::day5::part1(lines);
+    aoc24::day6::part2(lines);
     Ok(())
 
 }
