2d1bfb90f8c791f002a1a93262676198c688665b
Axel <ax1053ni-s@student.lu.se>
2024-12-13 09:55:39 +0100

    day 13

---
+++ src/aoc24/day13.rs
@@ -0,0 +1,82 @@
+use nalgebra::{Matrix2, Vector2};
+
+pub fn part1(lines: Vec<String>) {
+    let epsilon: f32 = 0.0001;
+    let no_blanks: Vec<&String> = lines.iter().filter(|line| *line != "").collect();
+    let mut sum = 0;
+    let mut a: (f32, f32) = (0.0, 0.0);
+    let mut b: (f32, f32) = (0.0, 0.0);
+    let mut prize: (f32, f32) = (0.0, 0.0);
+    for (i, str) in no_blanks.iter().enumerate() {
+        // println!("{}", i);
+        let nums: Vec<f32> = str
+            .split(|c: char| !c.is_numeric())
+            .filter(|x| *x != "")
+            .map(|x| x.parse::<f32>().unwrap())
+            .collect();
+        if i % 3 == 0 {
+            a = (nums[0], nums[1]);
+        }
+        if i % 3 == 1 {
+            b = (nums[0], nums[1]);
+        }
+        if i % 3 == 2 {
+            prize = (nums[0], nums[1]);
+
+            let A = Matrix2::new(a.0, b.0, a.1, b.1);
+            let v = Vector2::new(prize.0, prize.1);
+
+            let a_inv = A.try_inverse().unwrap();
+            let result = a_inv * v;
+
+            let a: i32 = result[0].round() as i32;
+            let b: i32 = result[1].round() as i32;
+            if (a as f32 -result[0]).abs() < epsilon && (b as f32-result[1]).abs() < epsilon{
+                sum += 3*a + b;
+            } 
+
+        }
+    }
+    println!("{}", sum);
+}
+
+
+pub fn part2(lines: Vec<String>) {
+    let epsilon: f64 = 0.01;
+    let no_blanks: Vec<&String> = lines.iter().filter(|line| *line != "").collect();
+    let mut sum = 0;
+    let mut a: (f64, f64) = (0.0, 0.0);
+    let mut b: (f64, f64) = (0.0, 0.0);
+    let mut prize: (f64, f64) = (0.0, 0.0);
+    for (i, str) in no_blanks.iter().enumerate() {
+        let nums: Vec<f64> = str
+            .split(|c: char| !c.is_numeric())
+            .filter(|x| *x != "")
+            .map(|x| x.parse::<f64>().unwrap())
+            .collect();
+        if i % 3 == 0 {
+            a = (nums[0], nums[1]);
+        }
+        if i % 3 == 1 {
+            b = (nums[0], nums[1]);
+        }
+        if i % 3 == 2 {
+            prize = (nums[0] + 10_000_000_000_000.0, nums[1] + 10_000_000_000_000.0);
+
+            let A = Matrix2::new(a.0, b.0, a.1, b.1);
+            let v = Vector2::new(prize.0, prize.1);
+
+            let a_inv = A.try_inverse().unwrap();
+            let result = a_inv * v;
+
+            let a: i64 = result[0].round() as i64;
+            let b: i64 = result[1].round() as i64;
+            if (a as f64 -result[0]).abs() < epsilon && (b as f64-result[1]).abs() < epsilon{
+                sum += 3*a + b;
+            } 
+
+        }
+    }
+    println!("{}", sum);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 #![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-#![allow(unused_mut)]
-#![allow(unused_assignments)]
+// #![allow(unused_variables)]
+// #![allow(unused_imports)]
+// #![allow(unused_mut)]
+// #![allow(unused_assignments)]
 mod aoc24; 
 
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Read};
+use std::io::{self, BufRead, BufReader};
 
 fn main() -> io::Result<()>{
 
-    let file_path: &str = "/home/axel/Documents/games/aoc24/src/input/day12.txt";
+    let file_path: &str = "/home/axel/Documents/games/aoc24/src/input/day13.txt";
     let file: File = File::open(file_path)?;
     let reader: BufReader<File> = io::BufReader::new(file);
     let lines: Vec<String> = reader.lines()
         .map(|line| line.unwrap()) // Handle Result by unwrapping (use ? if errors should propagate)
         .collect();
 
-    aoc24::day12::part2(lines);
+    aoc24::day13::part2(lines);
     Ok(())
 
 }
