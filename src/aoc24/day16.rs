f80ce3771786c0afed426063e8fdbda2f7db6cad
Axel <ax1053ni-s@student.lu.se>
2024-12-16 10:38:06 +0100

    some performance improvements

--- src/aoc24/day16.rs
+++ src/aoc24/day16.rs
@@ -1,7 +1,6 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque, BinaryHeap};
 
 use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 
 use itertools::Itertools;
 
@@ -24,6 +23,7 @@ pub fn part1(lines: Vec<String>) {
     let mut to_handle: VecDeque<(usize, usize, usize, i32, i32, i32)> = VecDeque::new();
     to_handle.push_front((start_pos.0, start_pos.1, 0, 0, 1, 0));
     while !to_handle.is_empty() {
+        //would be a lot more effecient to use a pq as in part 2 but this is effecient enough since i only need to find one path.
         to_handle = to_handle
             .iter()
             .sorted_by(|a, b| Ord::cmp(&a.2, &b.2))
@@ -73,32 +73,9 @@ pub fn part1(lines: Vec<String>) {
     println!("cost: {}, steps: {}", cost, s);
 }
 
-fn print_map(map: &Vec<Vec<char>>) {
-    println!();
-
-    for i in 0..map.len() {
-        for j in 0..map[i].len() {
-            print!("{}", map[i][j]);
-        }
-        println!();
-    }
-    println!();
-}
-
-fn rotate_clockwise(i: i32, j: i32) -> (i32, i32) {
-    (j, -i)
-}
-fn rotate_counter_clockwise(i: i32, j: i32) -> (i32, i32) {
-    (-j, i)
-}
-fn step(pos: (usize, usize), dir: (i32, i32)) -> (usize, usize) {
-    (
-        (pos.0 as i32 + dir.0) as usize,
-        (pos.1 as i32 + dir.1) as usize,
-    )
-}
 pub fn part2(lines: Vec<String>) {
-    let mut actual_cost = 100000000000000000;
+    //a high value to that is definetely higher than the best cost.
+    let mut cost_of_best_route = 100000000000000000;
     let mut map: Vec<Vec<char>> = Vec::new();
     let mut start_pos: (usize, usize) = (0, 0);
 
@@ -109,13 +86,11 @@ pub fn part2(lines: Vec<String>) {
         map.push(line.chars().collect());
     }
     print_map(&map);
-    let mut _cost = 0;
-    let mut _s = 0;
     //maybe needs to keep track of what the cost was
     let mut best_seats: HashSet<(usize, usize)> = HashSet::new();
     let mut visited: HashMap<(usize, usize, i32, i32), usize> = HashMap::new();
     let mut to_handle = BinaryHeap::new();
-    let binding = (start_pos.0, start_pos.1, 0, 0, 1, 0, Vec::new());
+    let binding = (start_pos.0, start_pos.1, 0, 0, 1, 0, vec![start_pos]);
     to_handle.push(Reverse((binding.3, binding)));
     while let Some(Reverse((_priority, (i, j, c, dir_i, dir_j, steps, path)))) = to_handle.pop() {
         if let Some(stored_c) = visited.get(&(i, j, dir_i, dir_j)) {
@@ -124,17 +99,20 @@ pub fn part2(lines: Vec<String>) {
             }
         }
         visited.insert((i, j, dir_i, dir_j), c);
-
+        //this path is already to long
+        if c > cost_of_best_route {
+            continue;
+        }
         // Check for the end condition
         if map[i][j] == 'E' {
             let mut tmp_path = path.clone();
-            tmp_path.push((i, j)); // Add 'E' to the path
-            if c <= actual_cost {
-                actual_cost = c;
+            tmp_path.push((i, j));
+            //only extend if this is the shortest path
+            if c <= cost_of_best_route {
+                cost_of_best_route = c;
                 best_seats.extend(tmp_path);
             }
             println!("found route");
-            // break;
         }
 
         // Process movements
@@ -171,6 +149,32 @@ pub fn part2(lines: Vec<String>) {
         map[*i][*j] = '0';
     }
     print_map(&map);
-    //+1 for offset startpos
-    println!("Path length: {}", best_seats.len() + 1);
+    println!("Path length: {}", best_seats.len());
+}
+
+
+
+fn print_map(map: &Vec<Vec<char>>) {
+    println!();
+
+    for i in 0..map.len() {
+        for j in 0..map[i].len() {
+            print!("{}", map[i][j]);
+        }
+        println!();
+    }
+    println!();
+}
+
+fn rotate_clockwise(i: i32, j: i32) -> (i32, i32) {
+    (j, -i)
+}
+fn rotate_counter_clockwise(i: i32, j: i32) -> (i32, i32) {
+    (-j, i)
+}
+fn step(pos: (usize, usize), dir: (i32, i32)) -> (usize, usize) {
+    (
+        (pos.0 as i32 + dir.0) as usize,
+        (pos.1 as i32 + dir.1) as usize,
+    )
 }
